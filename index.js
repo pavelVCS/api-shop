981f1abb6f2a526dd49ed69f7f7144f291247624
pavel <pavel@setupad.com>
2024-03-21 22:16:44 +0200

    add vercel

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/product', require('./routes/productGetterRoutes'));
 app.use('/product', require('./routes/productSetterRoutes'));
+app.get('/', (req, res) => res.send('Express on Vercel'));
 
 const connectDB = async () => {
   try {
